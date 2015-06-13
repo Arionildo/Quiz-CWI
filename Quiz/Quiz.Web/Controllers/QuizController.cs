288f91f6daa0b545df3b20e2168be903129f235f
talles Ludwig <tallesLudwig@gmail.com>
2015-06-13 18:23:26 -0300

    commit para pegar o merge

---
+++ Quiz/Quiz.Web/Controllers/QuizController.cs
@@ -0,0 +1,37 @@
+﻿using Quiz.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiz.Web.Controllers
+{
+    public class QuizController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Quiz
+        public ActionResult Index()
+        {
+
+            return View(db.Categorias.ToList());
+        }
+
+        //[AllowAnonymous]
+        //public JsonResult Perguntas()
+        //{
+            
+
+        //    return Json(new
+        //    {
+        //        restaurantes = todos
+        //    }, JsonRequestBehavior.AllowGet
+        //        );
+
+
+
+
+        //}
+    }
+}
