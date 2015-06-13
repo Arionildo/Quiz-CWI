9ace7bd94cc058c3000685cf14ba0294082f2ee2
talles Ludwig <tallesLudwig@gmail.com>
2015-06-13 17:10:51 -0300

    arrumei as descrições

--- Quiz/Quiz.Web/Migrations/Configuration.cs
+++ Quiz/Quiz.Web/Migrations/Configuration.cs
@@ -19,15 +19,15 @@ namespace Quiz.Web.Migrations
 
             context.Categorias.AddOrUpdate(
                 p => p.Nome,
-                new Categoria { Nome = "Ciência",               Descricao = "Mistura de todos os assuntos das mais diversas áreas." },
-                new Categoria { Nome = "Comidas E Bebidas",     Descricao = "Questões relacionadas à bandas, cantores e todos os estilos musicais." },
-                new Categoria { Nome = "Conhecimentos Gerais",  Descricao = "Tudo sobre filmes, atores e, quem sabe, até diretores!" },
-                new Categoria { Nome = "Esportes",              Descricao = "Dos elementos que nos compõe até a física que nos segura na terra." },
-                new Categoria { Nome = "Filme",                 Descricao = "Desde os ingredientes à diversos pratos e bebidas existentes ao redor do mundo." },
+                new Categoria { Nome = "Ciência",               Descricao = "Dos elementos que nos compõe até a física que nos segura na terra." },
+                new Categoria { Nome = "Comidas E Bebidas",     Descricao = "Desde os ingredientes à diversos pratos e bebidas existentes ao redor do mundo." },
+                new Categoria { Nome = "Conhecimentos Gerais",  Descricao = "Mistura de todos os assuntos das mais diversas áreas." },
+                new Categoria { Nome = "Esportes",              Descricao = "Todas as modalidades, do mais leve ao mais radical." },
+                new Categoria { Nome = "Filme",                 Descricao = "Tudo sobre filmes, atores e, quem sabe, até diretores!" },
                 new Categoria { Nome = "Geografia",             Descricao = "Lugares, climas, vegetações e tudo mais que a natureza tem a oferecer." },
-                new Categoria { Nome = "Jogos",                 Descricao = "Para aqueles quem não perdem uma novela, uma série ou até mesmo um programa de celebridades." },
-                new Categoria { Nome = "Música",                Descricao = "'Piece of cake' para os mais aficionados em jogos, seus personagens e suas histórias." },
-                new Categoria { Nome = "TV",                    Descricao = "Todas as modalidades, do mais leve ao mais radical." }
+                new Categoria { Nome = "Jogos",                 Descricao = "'Piece of cake' para os mais aficionados em jogos, seus personagens e suas histórias." },
+                new Categoria { Nome = "Música",                Descricao = "Questões relacionadas à bandas, cantores e todos os estilos musicais." },
+                new Categoria { Nome = "TV",                    Descricao = "Para aqueles quem não perdem uma novela, uma série ou até mesmo um programa de celebridades." }
             );
         }
     }
