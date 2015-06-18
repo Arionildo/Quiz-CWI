d7282980de54748392f694b4d0951032d94b13b5
Arionildo <arionildo6@gmail.com>
2015-06-18 03:31:19 -0300

    Fix Otimização desabilitada para teste

--- Quiz/Quiz.Web/App_Start/BundleConfig.cs
+++ Quiz/Quiz.Web/App_Start/BundleConfig.cs
@@ -34,6 +34,8 @@ namespace Quiz.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
